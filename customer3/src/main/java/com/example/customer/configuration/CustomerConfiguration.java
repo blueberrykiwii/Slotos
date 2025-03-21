cd26db789822b762e271cbdd0a41e40034856ed6
blueberrykiwii <149058740+blueberrykiwii@users.noreply.github.com>
2025-03-21 15:19:56 +0100

    Add files via upload

--- customer3/src/main/java/com/example/customer/configuration/CustomerConfiguration.java
+++ customer3/src/main/java/com/example/customer/configuration/CustomerConfiguration.java
@@ -2,8 +2,12 @@ package com.example.customer.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient  // Aktiviert die Eureka-Integration für Service Discovery
 public class CustomerConfiguration {
 
     public static void main(String[] args) {
