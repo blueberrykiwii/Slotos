99dd1699b96d4cbee3fe03b5e55db09745ea155f
blueberrykiwii <149058740+blueberrykiwii@users.noreply.github.com>
2025-03-22 10:46:33 +0100

    Add files via upload

--- order3/src/main/java/com/example/order/connector/OrderRepository.java
+++ order3/src/main/java/com/example/order/connector/OrderRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.example.order.component.structure.Order;
 
 @Repository
-public interface OrderRepository extends MongoRepository<Order, Integer> {
+public interface OrderRepository extends MongoRepository<Order, String> {
 
 	Optional<Order> findById(String id);
     // Hier kannst du zusätzliche Abfragen definieren, falls nötig
