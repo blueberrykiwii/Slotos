eb599db472520576c236e2d20cb88a4c82eda844
blueberrykiwii <149058740+blueberrykiwii@users.noreply.github.com>
2025-03-22 12:55:44 +0100

    Add files via upload

---
+++ eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EurekaServerApplication.class)
+public class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
